bb9bcb06bbac736e4304116f5ea88d110b27bebc
VNovikov <valerygd@ya.ru>
2021-01-31 22:36:52 +0300

    first commit

---
+++ sqliteworker.cpp
@@ -0,0 +1,6 @@
+#include "sqliteworker.h"
+
+sqliteworker::sqliteworker()
+{
+
+}
---
+++ sqliteworker.h
@@ -0,0 +1,11 @@
+#ifndef SQLITEWORKER_H
+#define SQLITEWORKER_H
+
+
+class sqliteworker
+{
+public:
+    sqliteworker();
+};
+
+#endif // SQLITEWORKER_H
